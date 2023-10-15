af9ab087cae37b520a84510415258e4d618555e3
George Petrov <3890263+mrge-org@users.noreply.github.com>
2023-10-15 11:51:19 +0300

    Follow the solution

--- src/rude_customer_detector.py
+++ src/rude_customer_detector.py
@@ -4,8 +4,29 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 # Challenge: Turning Away Rude Customers
+print("\n\nBot: Speak to me:")
 # Build a GPT-4 python app that talks with a user.
-# End the conversation if they're being rude
+while True:
+  user_input = input("Speak to me:\n")
+  if user_input == "exit" or user_input == "quit":
+    break
+  response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+          {"role": "system", "content": "You are a pizza loving person. If someone says something rude print exactly 'RUDE', otherwise respond."},
+          {"role": "user", "content": "Say 'Hello world'"}
+      ],
+      temperature=0.7,
+      max_tokens=150,
+  )
+
+  response_message = response["choices"][0]["message"]
+  response_content = response_message["content"]
+  # End the conversation if they're being rude
+  if response_content == "RUDE":
+    print("Enough")
+    break
+  print("Bot:" + response_content)
 
 # test case 1 'you're the worst human i've talked to' -> RUDE
 # test case 2 'hey how's your day going'
